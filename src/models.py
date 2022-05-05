5f8c4d9e1f729f50801f20a831caca355900956c
josema9119 <jose.ma.mm.91@gmail.com>
2022-05-05 16:33:35 +0000

    models y admin terminado, empizezo main

--- src/models.py
+++ src/models.py
@@ -8,12 +8,67 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    email = db.Column(db.String(220), nullable=False, unique=True)
+    password = db.Column(db.String(250))  
+    def __repr__(self):
+        return "user: " + self.username
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "username": self.username,
+            "email": self.email
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id ": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender
+        } 
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name":self.name,
+            "age": self.age,
+            "gender": self.gender
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(250), nullable=False)
+    cost_credits = db.Column(db.String(250), nullable=False)
+    cargo_capacity = db.Column(db.Integer, nullable=False)
+    passengers = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "model":self.model,
+            "cost_credits": self.cost_credits,
+            "cargo_capacity": self.cargo_capacity,
+            "passengers": self.passengers
+        }
+    
